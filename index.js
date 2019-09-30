69ff294f2c4d605d80d9d20bc32973d214096485
Paul M <paulmcadory@gmail.com>
2019-09-30 23:28:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var mus = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+var mus = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var inst = ["guitar", "bass", "guitar", "drums"];
 function theBeatlesPlay(mus, inst) {
   var array = {};
   for (var i = 0; i < mus.length; i++) {
