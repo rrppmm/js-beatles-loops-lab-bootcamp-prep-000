ceda5e8e6c15c5f11f1bad85d9767c98e5255860
Paul M <paulmcadory@gmail.com>
2019-09-30 23:38:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array;
 }
-function johnLennonFacts(facts) {
-  
+function johnLennonFacts(fact) {
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 }
