15080b1da4afed28fb799d63f40893594e1fd59f
Paul M <paulmcadory@gmail.com>
2019-09-30 23:24:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(mus, inst) {
   var array = {};
   for (var i = 0; i < mus.length; i++) {
-    array.push(`${mus[i]} plays ${inst[i]}`);
+    array.push(mus[i] + ' plays ' + inst[i]);
   }
   return array;
 }
