1b2298c722a03c04e59810550033b66973536cd6
Paul M <paulmcadory@gmail.com>
2019-09-30 23:36:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ["guitar", "bass", "guitar", "drums"];
 function theBeatlesPlay(musicians, instruments) {
-  var array = {};
+  var array = [];
   for (var i = 0; i < musicians.length; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
